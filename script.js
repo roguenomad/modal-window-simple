7ea49a5467bce21b2b1fb65ee71d8f6f70ad21b5
Nohely Garcia <connectedconfessions@gmail.com>
2026-07-25 13:44:25 -0700

    select modal text content

--- script.js
+++ script.js
@@ -7,3 +7,6 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 
 console.log(btnsOpenModal);
 
+for (let i = 0; i < btnsOpenModal.length; i++)
+    console.log(btnsOpenModal[i].textContent);
+
