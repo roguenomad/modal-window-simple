bf648c03b8692c22bce2a2ae1b14b0993bb44713
Nohely Garcia <connectedconfessions@gmail.com>
2026-07-25 14:21:03 -0700

    add close modal functionality

--- script.js
+++ script.js
@@ -8,5 +8,18 @@ const btnsOpenModal = document.querySelectorAll('.show-modal');
 console.log(btnsOpenModal);
 
 for (let i = 0; i < btnsOpenModal.length; i++)
-    console.log(btnsOpenModal[i].textContent);
+    btnsOpenModal[i].addEventListener('click', function()
+{
+    console.log('Button clicked')
+    modal.classList.remove('hidden');
+    overlay.classList.remove('hidden');
+});
+
+const closeModal = function () {
+    modal.classList.add('hidden');
+    overlay.classList.add('hidden');
+};
+
+btnCloseModal.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
 
